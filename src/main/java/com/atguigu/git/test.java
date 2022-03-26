27818c0671716ef9a31083664fae183dce17557a
Frank <Frank@atguigu.com>
2022-03-26 10:58:03 +0800

    first idea commit

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author Frank
+ * @create 2022-03-26 10:38
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello~World");
+
+
+    }
+
+}
