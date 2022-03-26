b1747542a24f18555732beb8151655181a464a63
Frank <Frank@atguigu.com>
2022-03-26 11:00:32 +0800

    second idea commit

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -9,6 +9,8 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello~World");
+        System.out.println("Hello~World1");
+        System.out.println("Hello~World2");
 
 
     }
